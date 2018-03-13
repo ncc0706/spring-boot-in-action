24ff1d87d6228fc8cc43042267fffc5de81c8ea5
niuyuxian <ncc0706@gmail.com>
2018-03-13 20:22:36 +0800

    Undertow Server

--- spring-boot-restful/src/main/java/io/arukas/SpringBootRestfulApplication.java
+++ spring-boot-restful/src/main/java/io/arukas/SpringBootRestfulApplication.java
@@ -2,11 +2,21 @@ package io.arukas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 @SpringBootApplication
 public class SpringBootRestfulApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootRestfulApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String index(ModelMap modelMap) {
+		modelMap.put("message", "Undertow server started.");
+		return "index";
 	}
 }
