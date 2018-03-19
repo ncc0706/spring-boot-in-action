da29b2ae3293d7801a05f233e1e23b1a8f230764
niuyuxian <ncc0706@gmail.com>
2018-03-19 18:49:40 +0800

    querydsl project

---
+++ spring-boot-querydsl/src/main/java/io/arukas/SpringBootQuerydslApplication.java
@@ -0,0 +1,12 @@
+package io.arukas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootQuerydslApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootQuerydslApplication.class, args);
+	}
+}
