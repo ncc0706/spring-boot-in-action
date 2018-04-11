7ddd00929902a64cde0b1fd8012cc98a856e5bf7
niuyuxian <ncc0706@gmail.com>
2018-04-11 20:35:25 +0800

    add db connect

--- spring-boot-security-jwt/src/main/java/io/arukas/SpringBootSecurityJwtApplication.java
+++ spring-boot-security-jwt/src/main/java/io/arukas/SpringBootSecurityJwtApplication.java
@@ -2,11 +2,13 @@ package io.arukas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class SpringBootSecurityJwtApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootSecurityJwtApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSecurityJwtApplication.class, args);
+    }
 }
