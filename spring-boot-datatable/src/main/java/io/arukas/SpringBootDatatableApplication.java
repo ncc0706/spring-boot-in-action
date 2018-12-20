648bfaba1bc3f5e08b949dd0ce35f1c90c184039
niuyuxian <ncc0706@gmail.com>
2018-12-20 09:25:15 +0800

    datatable example.

---
+++ spring-boot-datatable/src/main/java/io/arukas/SpringBootDatatableApplication.java
@@ -0,0 +1,14 @@
+package io.arukas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDatatableApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDatatableApplication.class, args);
+    }
+
+}
+
